5bd1023eb6b44e519b4d805ebc7836fc2f4abe3b
Saud CEO <saudceo@MacBook-Air-Saud.local>
2025-07-03 06:40:36 +0300

    Fix import from config.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
 import { backend } from "../config";
+
+
 export const socket = io(backend, {
   withCredentials: true,
   autoConnect: false, // We’ll connect manually after login
