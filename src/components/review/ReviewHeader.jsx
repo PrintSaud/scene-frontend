5ee8cb054550d92ee7f2fb58dd5086e5ac4b20a2
Saud CEO <saudceo@MacBook-Air-Saud.local>
2025-07-13 23:28:34 +0300

    Final frontend for Scene 🎬

--- src/components/review/ReviewHeader.jsx
+++ src/components/review/ReviewHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import StarRating from "../StarRating";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { format, formatDistanceToNowStrict, differenceInDays, parseISO } from "date-fns";
 import { useNavigate } from "react-router-dom";
 
 export default function ReviewHeader({ review, userId, onLike, onReply, onProfile }) {
